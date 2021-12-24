da4f3b0de3ec3d0036457a16220d5f77ebe623ac
Abdullohali <muhammadfoziljonov019@gmail.com>
2021-12-24 20:06:37 +0500

    Folder va Pagelar qo'shildi

---
+++ lib/screens/homepage/home_one_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeOne extends StatelessWidget {
+  const HomeOne({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
