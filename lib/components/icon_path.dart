9e431f4b8fac579d6d056b501aeff48da21517ef
Abdullohali <muhammadfoziljonov019@gmail.com>
2021-12-24 20:44:14 +0500

    iconlar qo'shildi va pathlarga ko'chirildi

---
+++ lib/components/icon_path.dart
@@ -0,0 +1,19 @@
+class Icons{
+  static const String arrowRight = "assets/icons/angle-right.svg";
+  static const String arrowLeft = "assets/icons/chevron-right.svg";
+  static const String arrowBottom = "assets/icons/Home-Search-Navi-Search-caret-down";
+  static const String favorite = "assets/icons/favorite.svg";
+  static const String mainUser = "assets/icons/Home-Navi-Main-User.svg";
+  static const String mainNotification = "assets/icons/Home-Navi-Main-Notification.svg";
+  static const String mainSearch = "assets/icons/Home-Navi-Main-Search.svg";
+  static const String mainhome = "assets/icons/Home-Navi-Main-home.svg";
+  static const String searchFilter = "assets/icons/Home-Search-Navi-Search-Filter.svg";
+  static const String hottelcon = "assets/icons/Hotelicon.svg";
+  static const String iconFavorite = "assets/icons/IconHeard.svg";
+  static const String iconHelp = "assets/icons/Iconhelp.svg";
+  static const String iconPayment = "assets/icons/IconPayment.svg";
+  static const String iconPromotions = "assets/icons/IconPromotions.svg";
+  static const String iconSettings = "assets/icons/IconSettings.svg";
+  static const String reactangle = "assets/icons/Reactangle.svg";
+  static const String roomPricing = "assets/icons/Room-Large-Pricing.svg";
+}
